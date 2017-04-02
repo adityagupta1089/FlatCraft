35afd1aca8e67008b064078bdf5e96ed3d31edbb
Aditya Gupta <adityagupta1089@gmail.com>
2017-04-02 15:10:07 +0530

    .

--- src/hud/FlatCraftHUD.java
+++ src/hud/FlatCraftHUD.java
@@ -33,8 +33,8 @@ public class FlatCraftHUD extends HUD {
 			if (cnt == 0) currItem = ie;
 			if (cnt++ < 8) {
 				ie.setListener(this);
-				ie.setPosition(X0 + (2 * cnt - 1) * deltaX, pY);
 				attachChild(ie);
+				ie.setPosition(X0 + (2 * cnt - 1) * deltaX, pY);
 				registerTouchArea(ie);
 			} else {
 				remainingTiles.add(ie);
